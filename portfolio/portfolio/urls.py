55e4f6193b39720f59aaf90a933072e437ecc357
Rubin <ayunran4@gmail.com>
2017-07-15 11:55:21 +0900

    Update urls.py

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('certificate.urls')),
     url(r'^admin/', admin.site.urls),   #앞부분은 url, 뒷부분이 실행사항
 ]
